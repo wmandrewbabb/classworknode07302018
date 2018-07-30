a37d6e676cdd203f275c88e0b4eaa8e3f330e33f
WMABABB <wm.andrew.babb@gmail.com>
2018-07-30 18:39:13 -0400

    huh

---
+++ classwork.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const port = 8000;
+
+function handleRequest(request, response) {
+
+    // console.log(request);
+    // console.log(response);
+    response.end("It works!" + request.url);
+
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, function() {
+    console.log(`Server listing on port ${port}`);
+    console.log("Awful teeth!");
+
+});
